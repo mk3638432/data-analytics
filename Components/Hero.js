0839ddd4619626e4c7c1255f0bbf7f2e89b94c4c
mk3638432@gmail.com <mk3638432@gmail.com>
2023-04-22 15:03:18 +0530

    second

--- Components/Hero.js
+++ Components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
        <div className='max-w-[800px] m-auto w-full h-screen  text-center flex  flex-col'>
         <p className='uppercase mt-[200px] font-bold text-green-400'>Growing With Data Analytics</p>
         <p className='text-3xl sm:text-6xl md:text-7xl font-bold py-2 '>Grow with data.</p>
-        <p className='font-bold text-xl sm:text-4xl md:text-5xl'>Fast, flexible financing for  
+        <p className='font-bold  text-xl sm:text-4xl md:text-5xl'>Fast, flexible financing for  
         <Typed className='pl-2' strings={["BTB" , "BTC" , "SASS"]} typeSpeed={120} backSpeed={120} loop/>
         </p>
         <p className='md:text-2xl font-bold text-gray-600 py-2 text-xl'>
